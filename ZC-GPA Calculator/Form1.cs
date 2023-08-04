3491f27b5bc30e882538612731be3e8cb7e04320
MoEssamKhattab <s-mo.essam@zewailcity.edu.eg>
2023-08-04 18:32:47 +0300

    small updates to the GUI

---
+++ ZC-GPA Calculator/Form1.cs
@@ -0,0 +1,15 @@
+namespace ZC_GPA_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void browseFileBtn_Click(object sender, EventArgs e)
+        {
+            tabs.SelectTab(tab2);
+        }
+    }
+}
