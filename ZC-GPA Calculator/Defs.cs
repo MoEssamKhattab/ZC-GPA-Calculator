cdbb205e1572b685fa8f4b21d5f58cbcdd304ffa
MoEssamKhattab <s-mo.essam@zewailcity.edu.eg>
2024-03-30 18:15:58 +0200

    handeling the case of SEM Military course

--- ZC-GPA Calculator/Defs.cs
+++ ZC-GPA Calculator/Defs.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace ZC_GPA_Calculator
 {
-    public enum CourseSubtype : byte { LECTURE, LAB };
+    public enum CourseSubtype : byte { LECTURE, LAB, SEM };
     public enum SemesterType : byte { Fall = 0, Spring = 1, Summer = 2, Summer1 = 3};
     public enum NonGPAGrades : byte 
     { 
